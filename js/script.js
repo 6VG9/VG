7baa4bef41a6d03f8d9dea50b9b96bcf635fef58
ComputerGenius9 <107172819+ComputerGenius9@users.noreply.github.com>
2022-08-19 23:28:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!')
+function calcSumm(num1, num2, more, less) {
+    let numSumm = num1 + num2;
+    
+    if (numSumm > 3) {
+        more();
+    } 
+    else {
+        less();
+    }
+}
+
+function showMoreMessage() {
+    console.log('Больше чем 3');
+}
+function showLessMessage() {
+    console.log('Меньше чем 3');
+}
+calcSumm(1, 1, showMoreMessage, showLessMessage);
+
